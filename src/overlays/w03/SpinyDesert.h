b0284813dd0e8430afa238bd4b294e6fbd894a26
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2026-05-31 23:00:06 -0500

    Add matches to Spiny Desert 34E840.c
    From PartyPlanner64/mp3.

---
+++ src/overlays/w03/SpinyDesert.h
@@ -0,0 +1,20 @@
+#include "common.h"
+#include "game/board.h"
+
+extern s16 D_800CE194_CED94[];
+
+extern RectF D_8011D598_366408_w03;
+
+extern struct event_table_entry *D_8011D1C4_366034_w03;
+extern struct event_table_entry *D_8011D23C_3660AC_w03;
+extern struct event_table_entry *D_8011D3D0_366240_w03;
+extern struct event_table_entry *D_8011D478_3662E8_w03;
+
+void func_8010622C_34F09C_w03(void);
+void func_80107A04_350874_w03(void);
+void func_8011A704_363574_w03(void);
+
+void func_800EBA60_FF680_shared_board(void *);
+void func_800F8D48_10C968_shared_board(void *);
+void func_800F8D6C_10C98C_shared_board(s32);
+void MBStart(s32);
