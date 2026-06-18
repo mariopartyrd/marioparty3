29f890485c8549ea798697ece92b5b0e103fdded
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2026-06-17 22:52:00 -0500

    Name the functions in sl.c

--- src/2.0L/audio/sl.c
+++ src/2.0L/audio/sl.c
@@ -1,9 +1,9 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/nonmatchings/2.0L/audio/sl", func_800771A0_77DA0);
+INCLUDE_ASM("asm/nonmatchings/2.0L/audio/sl", alInit);
 
-INCLUDE_ASM("asm/nonmatchings/2.0L/audio/sl", func_800771CC_77DCC);
+INCLUDE_ASM("asm/nonmatchings/2.0L/audio/sl", alClose);
 
-INCLUDE_ASM("asm/nonmatchings/2.0L/audio/sl", func_800771FC_77DFC);
+INCLUDE_ASM("asm/nonmatchings/2.0L/audio/sl", alLink);
 
-INCLUDE_ASM("asm/nonmatchings/2.0L/audio/sl", func_8007721C_77E1C);
+INCLUDE_ASM("asm/nonmatchings/2.0L/audio/sl", alUnlink);
